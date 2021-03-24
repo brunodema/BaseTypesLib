107e526f8b9b4a511174f80a87177a9441750f6a
Bruno Demantova <brunoeckdema@gmail.com>
2021-03-24 13:42:37 +0000

    add default constructor (missing from last commit)

--- src/Bounds.h
+++ src/Bounds.h
@@ -6,6 +6,7 @@ namespace base_types
 {
 struct Bounds
 {
+    Bounds() = default;
     Bounds(std::size_t lower_bound, std::size_t upper_bound) : lb(lower_bound), ub(upper_bound)
     {
         if (lb > ub)
