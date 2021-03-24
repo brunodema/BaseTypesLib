b777f8b80de15c0f22b5f481626a76a529da7efa
Bruno Demantova <brunoeckdema@gmail.com>
2021-03-24 13:39:49 +0000

    create default constructor, change bound check rule

--- src/Bounds.h
+++ src/Bounds.h
@@ -8,13 +8,13 @@ struct Bounds
 {
     Bounds(std::size_t lower_bound, std::size_t upper_bound) : lb(lower_bound), ub(upper_bound)
     {
-        if (lb >= ub)
+        if (lb > ub)
         {
             throw std::invalid_argument("The lower bound can not be bigger that then upper bound.");
         }
     }
 
-    std::size_t lb;
-    std::size_t ub;
+    std::size_t lb = 0;
+    std::size_t ub = 0;
 };
 } // namespace base_types
